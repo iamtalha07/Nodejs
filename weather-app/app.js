65b11ff005236627c07b3e59f8d8df52ad22db2c
Muhammad Talha <talha.ishaq@arpatech.com>
2022-11-04 18:09:54 +0500

    Code refactor: Destructuring and property shorthand

--- weather-app/app.js
+++ weather-app/app.js
@@ -2,22 +2,22 @@ const request = require('request')
 const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
-const location = process.argv[2]
+const address = process.argv[2]
 
-if(!location) {
+if(!address) {
     console.log('Please provide the location')
 } else {
-    geocode(location, (error, data) => {
+    geocode(address, (error, { latitude, longitude, location } = {}) => {
         if(error) {
             return console.log(error)
         }
     
-        forecast(data.latitude, data.longitude, (error, forecastData) => {
+        forecast(latitude, longitude, (error, forecastData) => {
             if(error) {
                 return console.log(error)
             }
     
-            console.log(data.location)
+            console.log(location)
             console.log(forecastData)
         })
     })

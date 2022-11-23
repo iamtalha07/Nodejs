16413f82bb5e74c483376a1c07176346fc52616c
Muhammad Talha <talha.ishaq@arpatech.com>
2022-11-23 23:26:34 +0500

    Serving up static assets

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,25 +1,12 @@
+const path = require('path')
 const express = require('express')
 
+
 const app = express()
+const publicDirectoryPath = path.join(__dirname, '../public')
 
-app.get('',(req,res) => {
-    res.send('<h1>Weather</h1>')
-})
+app.use(express.static(publicDirectoryPath))
 
-app.get('/help',(req,res) => {
-    res.send([{
-        name: 'Talha',
-        age: 25
-    }, 
-    {
-        name: 'Spiderman',
-        age: 25
-    }])
-})
-
-app.get('/about',(req,res) => {
-    res.send('<h1>About</h1>')
-})
 
 app.get('/weather',(req,res) => {
     res.send({

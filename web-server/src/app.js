fea45c2c6a00981ab8922849f4771fd0e0f3b5a7
Muhammad Talha <talha.ishaq@arpatech.com>
2022-11-24 00:17:29 +0500

    Serving up CSS,JS,Images and more

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -3,10 +3,9 @@ const express = require('express')
 
 
 const app = express()
+
 const publicDirectoryPath = path.join(__dirname, '../public')
-
 app.use(express.static(publicDirectoryPath))
-
 
 app.get('/weather',(req,res) => {
     res.send({

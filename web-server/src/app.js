5220ea48034505cf17699b22a3e84ef35dfd5351
Muhammad Talha <talha.ishaq@arpatech.com>
2022-11-24 21:01:28 +0500

    Dynamic pages with templating

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -3,9 +3,30 @@ const express = require('express')
 
 
 const app = express()
-
 const publicDirectoryPath = path.join(__dirname, '../public')
+
+app.set('view engine','hbs')
 app.use(express.static(publicDirectoryPath))
+
+app.get('',(req,res) => {
+    res.render('index',{
+        title:'Weather',
+        name: 'Muhammad Talha'
+    })
+})
+
+app.get('/about',(req,res) => {
+    res.render('about',{
+        title:'About',
+        name: 'Muhammad Talha'
+    })
+})
+
+app.get('/help',(req,res) => {
+    res.render('help',{
+        helpText: 'This is some helpful text'
+    })
+})
 
 app.get('/weather',(req,res) => {
     res.send({

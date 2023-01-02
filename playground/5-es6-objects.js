28cfed83ef218ca1c84ad00747558de7752a7d4e
Muhammad Talha <talha.ishaq@arpatech.com>
2023-01-03 00:07:30 +0500

    Default Function Parameter

--- playground/5-es6-objects.js
+++ playground/5-es6-objects.js
@@ -26,7 +26,7 @@ const product = {
 // const {label:productLabel, stock, rating = 5} = product
 // console.log(productLabel,stock,rating)
 
-const transaction = (type, { label, stock }) => {
+const transaction = (type, { label, stock = 0 } = {}) => {
     console.log(type)
     console.log(label)
     console.log(stock)

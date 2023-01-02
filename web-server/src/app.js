84b7d2f6bc8a74f75fa2261adb03b54d74d86814
Muhammad Talha <talha.ishaq@arpatech.com>
2023-01-02 23:47:50 +0500

    Building a JSON HTTP Endpoint

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,6 +1,8 @@
 const path = require('path')
 const express = require('express')
 const hbs = require('hbs')
+const geocode = require('./utils/geocode')
+const forecast = require('./utils/forecast')
 
 const app = express()
 
@@ -45,11 +47,33 @@ app.get('/weather',(req,res) => {
             error: 'You must provide an address'
         })
     }
-    res.send({
-        forecast: 'rainy',
-        location: 'Karachi',
-        address: req.query.address
+
+    geocode(req.query.address, (error, { latitude, longitude, location } = {}) => {
+        if(error) {
+            return res.send({
+                error: error
+            })
+        }
+    
+        forecast(latitude, longitude, (error, forecastData) => {
+            if(error) {
+                return res.send({
+                    error: error
+                })
+            }
+            res.send({
+                forecast: forecastData,
+                location,                       //location: location
+                address: req.query.address
+            })
+        })
     })
+
+    // res.send({
+    //     forecast: 'rainy',
+    //     location: 'Karachi',
+    //     address: req.query.address
+    // })
 })
 
 //Only single res is allowed
